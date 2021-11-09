ffd95e1974459eaf0bf9d45b6a4d91c0cbcdaa0f
Aleksandar <a.ivezic@yahoo.com>
2021-11-09 13:29:45 +0100

    Update API url

--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -1,5 +1,5 @@
 const getData = async () => {
-  const request = await fetch('v1/greeting.json');
+  const request = await fetch('http://localhost:3000/api/v1/greeting');
   const response = await request.json();
   const data = await response.greeting;
   return data;
